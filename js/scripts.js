f0e8537550f175fdf3073eb86629883dec502fc0
jaynara95 <jay.95nascimento@gmail.com.br>
2023-04-29 15:18:53 -0300

    feat: criacao de item de dropdoen no menu

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,15 @@ AOS.init({
     duration: 1000,
     once: true
 });
+
+const btnDropdown = document.querySelector('.js-btn-dropdown');
+const dropdown = document.querySelector('.js-dropdown');
+
+function openDropdown(e) {
+    e.preventDefault();
+   dropdown.classList.toggle('active');
+    
+}
+
+btnDropdown.addEventListener('click', openDropdown);
+dropdown.addEventListener('mouseleave', openDropdown);
